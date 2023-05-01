b7ab5fd5fe101585a9c69c50f98670ca8dbb9c55
sbe1337 <sic19001@student.mdu.se>
2023-05-01 23:24:30 +0200

    server and client
    not sure if multithread is broken now
    save each finger in separate json file (fix later if want)
    client.py will break if left open too long - requires beaglebone restart

--- src/beaglebone/client.py
+++ src/beaglebone/client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 ### using multithreading 
 '''
@@ -6,90 +6,55 @@ in this code the sensor data is read and saved in the memory where it is continu
 the sensor thread continuosly enqueue the sensor reading at 1Khz sampling frequency, and the send thread will send the data
 to the computer/laptop.
 it doesn't matter how fast we recieve the data as long as we measure it at the right frequency.
+
+If this script is kept open - beaglebone needs to be shutdown due to some overflow error!
 '''
 import queue
 import socket 
-import threading 
-import mmap
+import threading
 from time import sleep
-import json
-import keyboard
-import random as rd
-# Server address:port var
-#serverAddress = ("192.168.7.1", 8080)
-serverAddress = ("192.168.0.4", 8080) #test
 
-# Create a datagram socket
-tempSensorSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Set server address and create a datagram socket
+serverAddress = ("192.168.7.1", 8080)
+clientSensorSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Open the sensor file in read-only mode using a memory-mapped file
-
+# Analog input to read from file
 pin = 0
-filename = "sensorData.json"
-dataComplete = []
-#file = f"/sys/bus/iio/devices/iio:device0/in_voltage{pin}_raw"
-file = 'test.txt'
-with open(file, "r") as f:
-    mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-    # saving data into json file 
-    
-    def save_data(label):
-        global dataComplete
-        data_to_save = {'label': label, 'data': dataComplete}
-        with open(filename, 'w') as f:  # dumping to a file
-            json.dump(data_to_save, f)
-        print('Data saved with label:', label,"length=",len(dataComplete))
-        dataComplete.clear()
+file = "/sys/bus/iio/devices/iio:device0/in_voltage{}_raw".format(pin)
+f = open(file, "r")
 
-    # Define a function that reads the sensor data
-    def read_sensor_data():
-        while True:
-            # f.seek(0)
-            # data = f.read()
-            # Read the sensor data from the memory-mapped file
-            mm.seek(0)
-            data = mm.readline().strip()
-            #data = mm.read()
-            # Add the data to a queue to be sent over the socket
-            dataComplete.append(str(data))
+# Read sensor data from file and put in queue
+def read_sensor_data():
+    f.seek(0)
+    data = f.read().strip()
+    data_queue.put(data)
+    sleep(0.0005) #Play around with this value!
+        
+        
+# Send sensor data to server from queue
+def send_sensor_data():
+    data = data_queue.get()
+    clientSensorSocket.sendto(data.encode(), serverAddress)
+        
+    #dataIn,_ = clientSensorSocket.recvfrom(128)#128
 
-            sleep(0.001)
-            
-            
-    # Define a function that sends the sensor data over the socket
-    def send_sensor_data():
-        while True:
-            # Wait for data to be added to the queue
-            print(len(dataComplete))
-            sleep(1)
-            # Send the data over the socket
-            # tempSensorSocket.sendto(data, serverAddress)\
+      
+# Create a queue to hold the sensor data
+data_queue = queue.Queue()
 
-    def listen_keyboard():
-        while True:
-            if keyboard.is_pressed('1'):# thumb
-                # Wait for the key to be released
-                dataComplete.clear() # clearing data before saving into it
-                #sleep(0.01) # waiting for 10 miliseconds 
-                while keyboard.is_pressed('1'): 
-                    mm.seek(0)
-                    data = mm.readline().strip()
-                    dataComplete.append(str(data))
-                    print(len(dataComplete))
-                    sleep(0.001)
-                save_data('1')
-    
-    # Create a queue to hold the sensor data
-    data_queue = queue.Queue()
-    
-    # Create a thread to read the sensor data
-    #sensor_thread = threading.Thread(target=read_sensor_data)
-    
-    # Create a thread to send the sensor data
-    send_thread = threading.Thread(target=send_sensor_data)
-    keyboard_thread = threading.Thread(target=listen_keyboard)
-    
-    # Start the threads
-    #sensor_thread.start()
-    send_thread.start()
-    keyboard_thread.start()
+# Create a thread to read the sensor data
+sensor_thread = threading.Thread(target=read_sensor_data)
+send_thread = threading.Thread(target=send_sensor_data)
+
+# Start the threads
+send_thread.start()
+sensor_thread.start()
+
+while(True):
+    try:
+        read_sensor_data()
+        send_sensor_data()
+    except KeyboardInterrupt:
+        clientSensorSocket.close() #Close socket
+        print("Interrupt!")
+        break
--- src/server.py
+++ src/server.py
@@ -1,46 +1,74 @@
 #!/usr/bin/python
 
 import socket
-import datetime
-from time import sleep
-from datetime import datetime
+import json
+import keyboard
 
-#Set IP and Port of Actuator node receiving temperature
+#Set IP and Port of listening server
 ip      = "192.168.7.1"
 port    = 8080
 listeningAddress = (ip, port)
 
-dataCount = 0
-timeBefore = 0
-
 dataComplete = []
-dataSecond = [0 for i in range(1000)]
-filename = "sensorData.txt"
 
 #Make datagram socket using IPv4 addressing scheme and bind it 
 datagramSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 datagramSocket.bind(listeningAddress)
 
-#Liten to incoming messages, print  them and send response
-while(dataCount != 1000):
+name = input("Enter subject name: ")
+
+#Save data in JSON with labels
+def save_data(label):
+    global dataComplete
+    filename = str(name) + str(label) + "data.json" #Will save each finger in separate files, check if this can be improved
+    data_to_save = {'label': label, 'data': dataComplete}
+    with open(filename, 'w') as f:
+        json.dump(data_to_save, f, indent = 4)
+    print('Data saved with label:', label,"length: ",len(dataComplete))
+    dataComplete.clear()
+
+    
+#Receive new data to append
+def receive_data():
     emgVal,_ = datagramSocket.recvfrom(128)
     emgVal = float(emgVal.decode())
+    dataComplete.append(emgVal)
     
-    #For time stamps
-    if dataCount == 1:
-        timeBefore = datetime.now()
-    dataSecond.append(emgVal)
-    
-    dataCount += 1
-    if dataCount == 1000:
-        dataComplete.append(dataSecond)
+#Listen to keyboard press
+def listen_keyboard():
+    if keyboard.is_pressed('1'): #Thumb
+        dataComplete.clear() #Clear data before saving
+        while keyboard.is_pressed('1'): 
+            receive_data()
+        save_data('1')
+    elif keyboard.is_pressed('2'): #Index
+        dataComplete.clear()
+        while keyboard.is_pressed('2'): 
+            receive_data()
+        save_data('2')
+    elif keyboard.is_pressed('3'): #Middle
+        dataComplete.clear()
+        while keyboard.is_pressed('3'): 
+            receive_data()
+        save_data('3')
+    elif keyboard.is_pressed('4'): #Ring
+        dataComplete.clear()
+        while keyboard.is_pressed('4'): 
+            receive_data()
+        save_data('4')
+    elif keyboard.is_pressed('5'): #Little
+        dataComplete.clear()
+        while keyboard.is_pressed('5'): 
+            receive_data()
+        save_data('5')
 
-timeAfter = datetime.now() - timeBefore #comparing time before / after
-ms = timeAfter.total_seconds() * 1000 #in ms
+print("Hold number key 1-5 to record data in JSON (\'ctrl+c\' to exit)")
+print("1 = Thumb, 2 = Index, 3 = Middle, 4 = Ring, 5 = Little")
 
-print("We received " + str(dataCount) + "samples")
-
-print("It took " + str(ms) + "ms")
-
-with open(filename, 'w') as f: #dumping to a file
-    f.write(str(dataComplete))
+while(True):
+    try:
+        listen_keyboard()
+    except KeyboardInterrupt:
+        datagramSocket.close() #Close socket
+        print("Interrupt!")
+        break
