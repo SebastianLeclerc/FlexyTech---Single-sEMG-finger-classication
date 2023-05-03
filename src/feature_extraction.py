34b687867057026f230332949cdf96a1bb28f61c
Viktoriia Simakova <97298356+s-tori2go@users.noreply.github.com>
2023-05-03 10:11:23 +0200

    changes

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,23 +1,26 @@
+import math
+
+from src import np, pd
 def extract_features(data, overlap):
     """
     Extracts features from data and returns the fitted model object.
     :param data: data with signals
     :param overlap: overlaping space
     """
-       
-    # Time domain features: Mean absolute value (MAV), root mean square (RMS), waveform length (WL),zero crossings (ZC), variance (VAR)
+
+    # Time domain features
     features_names = ['Variance(VAR)', 'MeanAbsoluteValue(MAV)', 'RootMeanSquare(RMS)', 'WaveformLength(WL)', 'ZeroCrossings(ZC)']
+    var = []
     mav = []
     rms = []
     wl = []
-    zc = []
+    #zc = []
 
     # Iterate for each signal/window and calculate associated features
     for index in range(len(data)):
-        
         var.append(data.iloc[[index]].var(axis=1))
         mav.append(sum(abs(data.iloc[[index]].var(axis=1)))/len(data.iloc[[index]]))
-        rms.append(sqrt((data.iloc[[index]].mean(axis=1))**2))
+        rms.append(math.sqrt((data.iloc[[index]].mean(axis=1))**2))
         wl.append(sum(abs(data.iloc[[index]].diff(axis=1)))
         #zc.append(data.iloc[[index]].mean(axis=1))
     
@@ -25,10 +28,10 @@ def extract_features(data, overlap):
     mav = np.array(mav)
     rms = np.array(rms)
     wl = np.array(wl)
-    zc = np.array(zc)
+    #zc = np.array(zc)
 
     matrix = np.column_stack((mav, rms, wl, zc))
-    df_features = pd.DataFrame(matrix, columns = feature_names)
+    df_features = pd.DataFrame(matrix, columns = features_names)
     data = pd.concat([data, df_features], axis=1)
 
     return data
