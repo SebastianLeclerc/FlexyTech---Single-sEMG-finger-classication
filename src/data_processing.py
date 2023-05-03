e177345bc536ad3018a6e549391f6cd3da3e31c6
nazem9 <69907990+nazem9@users.noreply.github.com>
2023-05-03 09:47:25 +0200

    added a few lines for features extraction methods

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,7 +1,11 @@
 from src import pd
 
 def preprocess_data(file_path):
-    """Loads data from file and returns processed data."""
+    """
+    to do:
+        inputs data raw , labels, time stamps
+        returns processed data raw in floating point,the data must be centered at zero, labels, time_stamps (maybe changed or verified)
+    """
     df = pd.read_csv(file_path)
     # To Do
     return df
--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,4 +1,26 @@
-def extract_features(data):
-    """Extracts features from data and returns the fitted model object."""
-    # To Do
-    return data
+from combine_data import combine_data
+from librosa.feature import zero_crossing_rate
+import numpy as np
+import pandas as pd
+data,label,_= combine_data('./../data') # some tests.
+def extract_features(data,label,features_no,*features_funcs,):
+    """Extracts features from data and returns the fitted model object.
+    create,recieves the processed and returns the labeled dataset for training the model.
+    data : processed data np.array
+    labels : lable for the data np.array
+    features_no : number of measurements from eac feature
+    feature_funcs : list or iterable feature extraction functions
+    the feature function should accept the data as the first argument, hop_length for the second argumet,
+    
+    """
+    print(type(data))
+    features = {}
+    for func in features_funcs:
+        features[func.__name__] = []
+        for i,samples in enumerate(data):
+            features[func.__name__].append(func(y = samples,hop_length=1+int((len(samples))/features_no))[0])
+    features['labels'] = label
+    return pd.DataFrame(features)
+
+if __name__ == '__main__':
+    print(extract_features(data,label,3,zero_crossing_rate))
