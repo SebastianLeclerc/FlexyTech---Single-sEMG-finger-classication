98250497cf659a84a34912e2c76ffcbaea83ffbc
GitHubAlejandroDR <100710035+GitHubAlejandroDR@users.noreply.github.com>
2023-05-03 10:04:26 +0200

    main_adapted_metrics_training

--- src/main.py
+++ src/main.py
@@ -20,12 +20,16 @@ X, y = extract_features(processed_data)
 
 models = ['', 'KNN', 'Support Vector Machine']
 trained_models = {}
+# Dataframe which store metrics for each model
+eval_results = pd.DataFrame()
 for model in models:
     trained_models[model] = train_model(model, df_train)
 
 for model, trained_model in trained_models.items():
-    best_model = evaluate_model(model, trained_model)
+    eval_results = pd.concat([eval_results, evaluate_model(model, trained_model, df_test)], axis=1)
 
+# Select best model based on X metric 
+best_model = trained_models[str(eval_results['metric_selected'].idxmax())]
 model_filename = f"{best_model}_{time.strftime('%Y-%m-%d')}.pkl"
 model_file_path = os.path.join("../models", model_filename)
 with open(model_file_path, 'wb') as f:
