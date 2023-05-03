991e8b9991759d79adfdcd1b1c918b2b25d3da7c
GitHubAlejandroDR <100710035+GitHubAlejandroDR@users.noreply.github.com>
2023-05-03 10:00:23 +0200

    firsts_metrics_v1.0

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,4 +1,25 @@
-def evaluate_model(model, trained_model):
+def evaluate_model(model, trained_model, df_test):
     """Evaluates a machine learning model and returns the evaluation metrics."""
-    # To Do
-    return model
+
+    # Split features and classes
+    X_test = df.iloc[:, :-1]
+    y_test = df.iloc[:, -1:]
+
+    # Metrics: 
+    metrics = ['accuracy', 'precision', 'roc_auc', 'recall', 'f1_score', 'mean_absolute_error']
+
+    # Model prediction
+    y_pred = model.predict(X_test)
+
+    # Dataframe metric results
+    results = pd.DataFrame(index = [model], columns = metrics)
+
+    # Calculate metrics
+    results['accuracy'] = accuracy_score(y_test, y_pred)
+    results['precision'] = precision_score(y_test, y_pred)
+    results['roc'] = roc_auc_score(y_test, y_pred, multi_class='ovr')
+    results['recall'] = recall_score(y_test, y_pred)
+    results['f1_score'] = f1_score(y_test, y_pred)
+    results['mean_absolute_error'] = mean_absolute_error(y_test, y_pred)
+    
+    return results
