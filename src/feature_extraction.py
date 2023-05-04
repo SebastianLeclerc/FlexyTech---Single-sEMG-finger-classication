5277cf68549d6606b3ab21b0f8e29a2362c3230b
nazem9 <69907990+nazem9@users.noreply.github.com>
2023-05-04 11:47:11 +0200

    feature extraction bug fixes and adding features

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,11 +1,22 @@
 from combine_data import combine_data
-from librosa.feature import zero_crossing_rate
+import librosa
+from math import floor
 import numpy as np
 import pandas as pd
 
+def _split_columns(df:pd.DataFrame,column_name:str,length:int) ->pd.DataFrame:
+    '''
+    a simple function that split the column of list to multiple columns
+    and return a data frame object
+    '''
+    column_names = [column_name + str(i) for i in range(1,length+2)]
+    #, columns=column_names
+    return pd.DataFrame(df[column_name].to_list(),columns=column_names)
 
 
-def frame_v1(data,no_frames,overlapping_percetage = 0.25):
+
+
+def frame_v1(y,no_frames,overlapping_percetage = 0.25):
     """            
     frame function
     based on the librosa framing function it returns a matrix with overlapping frames from the samples.
@@ -26,8 +37,10 @@ def frame_v1(data,no_frames,overlapping_percetage = 0.25):
 
     # calculating the hop length from the overlapping percentage -need editing.
     hop_length = int((1-overlapping_percetage)*len(data)/no_frames) 
-    return librosa.util.frame(data,frame_length=no_frames,hop_length = hop_length)
-def framer_v2(data,frame_length,hop_length):
+    return librosa.util.frame(y,frame_length=no_frames,hop_length = hop_length)
+
+
+def framer_v2(y,frame_length,hop_length):
     """            
     frame function
     based on the librosa framing function it returns a matrix with overlapping frames from the samples.
@@ -44,18 +57,28 @@ def framer_v2(data,frame_length,hop_length):
       [2, 4, 6]]
 
     """
-    return librosa.util.frame(data,frame_length=frame_length,hop_length = hop_length)
+    return librosa.util.frame(y,frame_length=frame_length,hop_length = hop_length)
 
 
 ### redefining features for the feature extraction function.###
-def mean(data,frame_length,hop_length):
-    return framer_v2(data,frame_length).mean(axis=0)
+def mean(y,frame_length,hop_length):
+    return framer_v2(y,frame_length,hop_length).mean(axis=0)
 
-def variance(data,frame_length,hop_length):
-    return framer_v2(data,frame_length,hop_length).variance(axis=0)
+def variance(y,frame_length,hop_length):
+    return framer_v2(y,frame_length,hop_length).var(axis=0)
 
+def zero_crossing_rate(y,frame_length,hop_length):
+    frames = framer_v2(y,frame_length,hop_length)
+    signs = np.sign(frames)
+    difference = np.diff(signs,axis=0)
+    absolute = np.abs(difference)
+    means= np.mean(absolute,axis=0)
+    return means / 2
 
-def extract_features(data,label,features_no,*features_funcs,):
+def iemg(y,frame_length,hop_length):
+    return np.abs(framer_v2(y, frame_length, hop_length)).sum(axis=0)
+
+def extract_features(data,label,features_no,overlapping_percentage=0.25,features_funcs=[]):
     """Extracts features from data and returns the fitted model object.
     create,recieves the processed and returns the labeled dataset for training the model.
     data : processed data np.array
@@ -71,15 +94,16 @@ def extract_features(data,label,features_no,*features_funcs,):
         column_names.append(func.__name__)
         features[func.__name__] = []
         for samples in data:
-
-            features[func.__name__].append(func(y = samples,hop_length=1+int((len(samples))/features_no))[0])
+            features[func.__name__].append(func(y = samples,frame_length=floor(len(data)/features_no-1),hop_length=int((1-overlapping_percentage)*floor((len(samples))/features_no))))
     features['labels'] = label
+
     df = pd.DataFrame(features)
-    for func in column_names:
-        df[[name+str(j) for name in column_names for j in range(1,features_no+1)] ] = pd.DataFrame(df[func].tolist(), index= df.index)
-        df.drop(columns=column_names,inplace=True)
+    print(df)
+    labels = df.labels
+    df = pd.concat([_split_columns(df,column_name=column,length=features_no) for column in column_names],axis=1)
+    df['label'] = labels
     return df
 
 if __name__ == '__main__':
     data,label,_= combine_data('./../data') # some tests.
-    print(extract_features(data,label,4,zero_crossing_rate,))
+    print(extract_features(data = data,label = label,features_no= 3,overlapping_percentage=0.25,features_funcs=[mean,zero_crossing_rate,iemg]))
