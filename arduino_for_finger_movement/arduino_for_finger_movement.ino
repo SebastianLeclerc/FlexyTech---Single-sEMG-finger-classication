4629ff5de56ce83b2f8ef47f6d821f14111a0a27
Stickkan <98158805+Stickkan@users.noreply.github.com>
2023-05-12 10:36:51 +0200

    Created a code for the arduino to read from three different pins and depending on which pin has a HIGH input the hand will move that (or those) fingers. I have not tested if it actually work with the beaglebone since that is worked on by Nazem and Sebastian.

---
+++ arduino_for_finger_movement/arduino_for_finger_movement.ino
@@ -0,0 +1,51 @@
+#include <Servo.h>
+
+Servo index;  // create servo object to control a servo
+Servo middle;
+Servo ring_little;
+
+int sensorPin1 = 2;  //Replace with the correct pin number.
+int sensorPin2 = 3;
+int sensorPin3 = 4;
+int sensorValue1;  //Sensor value from sensorPin1
+int sensorValue2;
+int sensorValue3;
+
+int pos = 0;  // variable to store the servo position
+
+void setup() {
+  index.attach(9);  // attaches the servo on pin 9 to the servo object
+  middle.attach(10);
+  ring_little.attach(11);
+  Serial.begin(9600);
+  pinMode(sensorPin1, INPUT);
+  pinMode(sensorPin2, INPUT);
+  pinMode(sensorPin3, INPUT);
+}
+
+void loop() {
+
+  sensorValue1 = digitalRead(sensorPin1);
+  sensorValue2 = digitalRead(sensorPin2);
+  sensorValue3 = digitalRead(sensorPin3);
+
+  if (sensorValue1 == HIGH) {
+    //send which pin to the 'move function' in order to make the correct finger move
+    moveFinger(index);
+  } else if (sensorValue2 == HIGH) {
+    moveFinger(middle);
+  } else if (sensorValue3 == HIGH) {
+    moveFinger(ring_little);
+  }
+}
+
+void moveFinger(Servo finger) {
+  for (pos = 0; pos <= 160; pos += 2) {  // goes from 0 degrees to 160 degrees in steps of 2 degree
+    finger.write(pos);                   // tell servo to go to position in variable 'pos'
+    delay(5);
+  }
+  for (pos = 160; pos >= 0; pos -= 2) {  // goes from 160 degrees to 0 degrees
+    finger.write(pos);                   // tell servo to go to position in variable 'pos'
+    delay(5);                            // waits 5ms for the servo to reach the position
+  }
+}
