7da870612c3e5d0aec9eaa41988514d01a8f252b
GitHubAlejandroDR <alejandrodominguezrecio@uma.es>
2023-05-17 09:30:23 +0200

    naz_data files + modified combine_data + jupyter process data / sliding window / feature extraction

--- src/combine_data.py
+++ src/combine_data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import re
 def combine_data(path:str):
     """
     this script will read the json files and structures it itno X,y for preprocessing and feature extraction.
@@ -14,9 +15,11 @@ def combine_data(path:str):
         if file_name.endswith("json"):
             df = pd.read_json(path + '/'+file_name)
             data.append(np.asarray(df.data.values,dtype=np.float32))
-            labels.append(df.label.unique()[0])
-            time_stamps.append(df.time)
+            #labels.append(df.label.unique()[0])
+            labels.append(re.findall(pattern=r'[A-Z]\w+',string=file_name)[0])
+            #time_stamps.append(df.time)
     data = np.array(data)
     labels = np.array(labels)
     time_stamps = np.array(time_stamps)
     return data,labels,time_stamps
+#print(combine_data(r'data\naz_data')[1])
---
+++ src/packages.py
@@ -0,0 +1,21 @@
+# Built-in packages
+import os
+import time
+import pickle
+import json
+import random
+
+# Third-party packages
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split, RepeatedKFold, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, roc_auc_score, recall_score, f1_score, mean_absolute_error
+from sklearn.svm import SVC
+
+# Package modules
+from data_processing import preprocess_data
+from feature_extraction import extract_features
+from model_training import train_model
+from model_evaluation import evaluate_model
