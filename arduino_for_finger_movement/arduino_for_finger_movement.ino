5f479c0c579ccaa0fe5272c5db7fe69c379783db
sbe1337 <123743404+sbe1337@users.noreply.github.com>
2023-05-23 15:49:07 +0200

    fixed robotic finger
    On prediction from Beaglebone GPIO out -> reads Arduino GPIO in -> Move the corresponding finger

--- arduino_for_finger_movement/arduino_for_finger_movement.ino
+++ arduino_for_finger_movement/arduino_for_finger_movement.ino
@@ -3,8 +3,8 @@
 Servo index, middle, ring_little;  // create servo object to control a servo
 
 int sensorPin1 = 2;  //Replace with the correct pin number.
-int sensorPin2 = 3;
-int sensorPin3 = 4;
+int sensorPin2 = 4;
+int sensorPin3 = 7;
 int sensorValue1, sensorValue2, sensorValue3;  //Sensor value from sensorPin1
 
 int pos = 0;  // variable to store the servo position
@@ -19,6 +19,11 @@ void setup() {
   pinMode(sensorPin1, INPUT);
   pinMode(sensorPin2, INPUT);
   pinMode(sensorPin3, INPUT);
+
+
+  index.write(0);
+  middle.write(0);
+  ring_little.write(180); 
 }
 
 void loop() {
@@ -26,26 +31,38 @@ void loop() {
   sensorValue1 = digitalRead(sensorPin1);
   sensorValue2 = digitalRead(sensorPin2);
   sensorValue3 = digitalRead(sensorPin3);
-
+  
   //The threshold for LOW(logic 0)(Expressed in Volt = 2.35V) is 0.3 Vcc and the threshold for HIGH (Logic 1)(Expressed in Volt = 2.60V) is 0.6Vcc
   //Maxiumum output voltage of the beaglebone green is 3.3V
   if (sensorValue1 == HIGH) {
     //send which pin to the 'move function' in order to make the correct finger move
-    moveFinger(index);
+    moveFingerOne(index);
   } else if (sensorValue2 == HIGH) {
-    moveFinger(middle);
+    moveFingerOne(middle);
   } else if (sensorValue3 == HIGH) {
-    moveFinger(ring_little);
+    //For ring and middle finger
+    moveFingerTwo(ring_little);
   }
 }
 
-void moveFinger(Servo finger) {
-  for (pos = 0; pos <= 160; pos += 2) {  // goes from 0 degrees to 160 degrees in steps of 2 degree
-    finger.write(pos);                   // tell servo to go to position in variable 'pos'
-    delay(5);                            // delays for 5 ms 
-  }
-  for (pos = 160; pos >= 0; pos -= 2) {  // goes from 160 degrees to 0 degrees
-    finger.write(pos);                   // tell servo to go to position in variable 'pos'
-    delay(5);                            // waits 5ms for the servo to reach the position
-  }
+void moveFingerOne(Servo finger) {
+    for (pos = 0; pos <= 160; pos += 2) {  // goes from 0 degrees to 160 degrees in steps of 2 degree
+      finger.write(pos);                   // tell servo to go to position in variable 'pos'
+      delay(5);                            // delays for 5 ms 
+    }
+    for (pos = 160; pos >= 0; pos -= 2) {  // goes from 160 degrees to 0 degrees
+      finger.write(pos);                   // tell servo to go to position in variable 'pos'
+      delay(5);                            // waits 5ms for the servo to reach the position
+    }
+}
+
+void moveFingerTwo(Servo finger) {
+    for (pos = 160; pos >= 0; pos -= 2) {
+      finger.write(pos);
+      delay(5);
+    }
+    for (pos = 0; pos <= 160; pos += 2) {
+      finger.write(pos);
+      delay(5);
+    }
 }
